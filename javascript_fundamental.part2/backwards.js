3201c4765029143a962efa714bc190e9f62dd54f
vandana <vandana19@navgurukul.org>
2022-07-01 17:51:41 +0530

    backward

---
+++ javascript_fundamental.part2/backwards.js
@@ -0,0 +1,12 @@
+const jonas = [
+    'jonas',
+    'Schmedtamma',
+    2037-1991,
+    'teacher',
+    ['Michael', 'peter', 'steven'],
+    true
+];
+
+for(let i = jonas.length-1; i >= 0; i--){
+    console.log(i, jonas[i])
+}
